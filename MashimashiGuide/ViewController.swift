4a2eea904c97348921f82081495ab04aefaba31c
almashY <kshow2171@gmail.com>
2025-06-18 00:30:55 +0900

    InitialCommit

---
+++ MashimashiGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MashimashiGuide
+//
+//  Created by 工藤翔太 on 2025/06/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
