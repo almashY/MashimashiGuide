29fd9ff4ff593904feb7060bb824a303189d1b35
almashY <kshow2171@gmail.com>
2025-10-27 00:42:59 +0900

    #0011 タブバー、タブバーアイテムのカラーをコード上で変更

--- MashimashiGuide/MainViewController.swift
+++ MashimashiGuide/MainViewController.swift
@@ -14,11 +14,35 @@ class MainViewController: UITabBarController {
         
         // 基本ルール画面を初期選択にする
         self.selectedIndex = 0
-    
+        
+        // タブバーのカラーを変更する
+        changeTaBarColor(.white)
     }
     
-    
-
+    func changeTaBarColor(_ barColor: UIColor) {
+        // UITabBarの外観を設定
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.backgroundColor = barColor
+        
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        if #available(iOS 15.0, *) {
+            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        }
+        
+        // タブバーアイテムの色
+        UITabBar.appearance().tintColor = UIColor.red
+        UITabBar.appearance().unselectedItemTintColor = UIColor.darkGray
+        
+        // 既存のタブバーに変更を反映させる
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            for view in window.subviews {
+                view.removeFromSuperview()
+                window.addSubview(view)
+            }
+        }
+        
+    }
     
 }
 
